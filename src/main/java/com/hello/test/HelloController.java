ac91fad216aabd82fc945b5d3081e14d1f56b632
jyothi2466 <settij33@gmail.com>
2022-06-14 11:24:45 +0530

    initial source code

---
+++ src/main/java/com/hello/test/HelloController.java
@@ -0,0 +1,14 @@
+package com.hello.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	public String sayHello() {
+		return "Welcome to Hello World!....";
+	}
+
+}
