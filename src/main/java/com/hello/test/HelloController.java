82ddeafc365fd92f1a6363879f83cd467e1fc1df
jyothi2466 <settij33@gmail.com>
2022-06-14 23:45:38 +0530

    Adding Method

--- src/main/java/com/hello/test/HelloController.java
+++ src/main/java/com/hello/test/HelloController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-	
+
 	@GetMapping("/say")
 	public String sayHello() {
 		return "Welcome to Hello World!....Hello Satheesh Chepuri";
+	}
+
+	@GetMapping("/hi")
+	public String say() {
+		return "Welcome to New World";
 	}
 
 }
